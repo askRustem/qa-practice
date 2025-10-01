9796ca56c9c0da81c64c8162b57127c42cfb6e26
askRustem <askerovrv.work@gmail.com>
2025-10-01 14:35:14 +0500

    upd main

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,12 +3,15 @@ package guru.qa;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private static final String scr = "Hi everyone!";
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and buy!!");
+        System.out.printf(scr);
 
-        for (int i = 0; i <= 15; i++) {
+        for (int i = 17; i <= 99; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
